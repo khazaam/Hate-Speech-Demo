c81265065e750cbb9a11aa1a6f7918f4ca6be547
Nuubles <35395588+Nuubles@users.noreply.github.com>
2021-04-29 19:06:21 +0300

    Refactor code

--- HateNew.py
+++ HateNew.py
@@ -20,17 +20,7 @@ from nltk import FreqDist
 from nltk.util import ngrams
 from nltk.stem.snowball import SnowballStemmer
 
-##parse hate terms from vocab
-hateList = []
-with open('./hate.json', 'r', encoding="utf-8") as file:
-	hateList = json.load(file)
-
-transformList = {}
-with open('./format.json', 'r', encoding="utf-8") as file:
-	transformList = json.load(file)
-
 ##helpers
-
 def clearFormat(text):
 	text = text.lower()
 	for key in transformList:
@@ -41,26 +31,26 @@ def clearFormat(text):
 
 ##hateSearch starts from here
 ##Hate search is not working currectly, input works, but gives false positive everytime.
-def hateSearch(url):
+def hateSearch(url, hateList, transformList):
     resp = requests.get(url)
 
     ##add input word spot
-    
+
     ##testing with one word at the time
     ##if you wish to test with just one word you can change this to the word you want to find
     testi_sana = ("subhuman")
-    
+
     html_page = resp.content
     soup = BeautifulSoup(html_page, 'html.parser')
     ##TODO make a similar list or file where you can get the textFind contents
     textFind = soup.find_all(text=True)
-    
+
      ##Note,  use [] list as a finding stuff, but {} does not find any
     hateFound = soup.body.find_all(text=re.compile('.*{0}.*'.format(testi_sana)),recursive=True)
-    
+
     print('Hate word found "{0}" {1} time\n'.format(testi_sana, len(hateFound)))
-    
-    
+
+
     for content in hateFound:
         words = content.split()
         for index, word in enumerate(words):
@@ -73,12 +63,19 @@ def hateSearch(url):
                 if index != len(words) -1:
                     after = words[index +1]
                 print('\word before: "{0}", word after: "{1}"').format(before, after)
-                return 
+                return
 
 
+def readJsonFile(location):
+	js = []
+	with open(location, 'r', encoding="utf-8") as file:
+		js = json.load(file)
+	return js
 
 if __name__ == "__main__":
 	if(len(sys.argv) != 2):
 		print("Invalid amount of arguments. When running the program, input 1 url to search the hate speech from")
 	else:
-		hateSearch(sys.argv[1])
+		hateList = readJsonFile('./hate.json')
+		transformList = readJsonFile('./format.json')
+		hateSearch(sys.argv[1], hateList, transformList)
